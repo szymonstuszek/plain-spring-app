6cc7994457bc924ba0088e61708843a243e8fbf8
szymon <szymonstuszek@gmail.com>
2022-03-22 05:22:58 +0100

    plain spring mvc with embedded tomcat

--- src/main/java/pl/devs/mentoring/PlainSpringApp.java
+++ src/main/java/pl/devs/mentoring/PlainSpringApp.java
@@ -1,28 +1,30 @@
 package pl.devs.mentoring;
 
+import org.apache.catalina.startup.Tomcat;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("pl.devs.mentoring")
-public class PlainSpringApp extends AbstractAnnotationConfigDispatcherServletInitializer {
+public class PlainSpringApp implements WebMvcConfigurer {
+    public static void main(String[] args) throws Exception {
+        String docBase = (System.getProperty("java.io.tmpdir"));
+        Tomcat server = new Tomcat();
+        server.setBaseDir(docBase);
+        server.setPort(8080);
+        server.getConnector();
+        server.addContext("", docBase);
 
-	@Override
-	protected String[] getServletMappings() {
-		return new String[]{"/"};
-	}
+        AnnotationConfigWebApplicationContext spring = new AnnotationConfigWebApplicationContext();
+        spring.register(PlainSpringApp.class);
+        server.addServlet("", "dispatcher", new DispatcherServlet(spring)).addMapping("/");
 
-	@Override
-	protected Class<?>[] getRootConfigClasses() {
-		return new Class<?>[]{PlainSpringApp.class};
-	}
-
-	@Override
-	protected Class<?>[] getServletConfigClasses() {
-		return new Class[0];
-	}
-
+        server.start();
+        server.getServer().await();
+    }
 }
